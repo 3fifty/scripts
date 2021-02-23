b1c1a084a06c97bc8aa4f75bf42ab0e43d59d034
Léon <admin@familieboers.onmicrosoft.com>
2021-02-23 17:46:02 +0100

    updated version number

--- 01-New-MDPowerShellScripts/New-MDPowerShellScripts.ps1
+++ 01-New-MDPowerShellScripts/New-MDPowerShellScripts.ps1
@@ -21,7 +21,7 @@
 Include the TOC from the Azure DevOps wiki to the markdown files
 
 .NOTES
-    Version:        1.0.0;
+    Version:        1.0.1;
     Author:         3fifty | Maik van der Gaag | Leon Boers;
     Creation Date:  20-04-2020;
     Purpose/Change: Initial script development;
